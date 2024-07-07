b7d222d75d0c3241cb8ef6fa4fabb63e54e5f3af
subharadhe1234 <subhadippratihar09@gmail.com>
2024-07-07 14:19:33 +0530

    second commit'

---
+++ src/exception.py
@@ -0,0 +1,4 @@
+import sys
+'''The sys library in Python is a built-in module that provides information about the current Python 
+interpreter and allows you to manipulate different parts of the Python runtime environment.'''
+#def error_message_detail(error,error_detail:sys):
